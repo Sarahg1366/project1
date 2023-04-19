d061484ca167c269d315a15a8b8362cd725fcc93
sarah <sarah.amarila@gmail.com>
2023-04-19 15:51:26 +0800

    WebsitePOM

---
+++ WebsitePOM/pages/HomePage.js
@@ -0,0 +1,15 @@
+//master file: unientPOM.spec.js
+
+export class HomePage {
+    constructor(page) {
+     this.page = page;
+    
+   //page title locator
+    this.header = page.locator("h1");
+    
+   //Homepage logo locator
+    this.logo = page.getByRole('link', { name: 'logo development - online branding solutions' });
+
+    }
+    
+    }
---
+++ WebsitePOM/pages/LoginPage.js
@@ -0,0 +1,41 @@
+//master file: unientPOM.spec.js
+
+export class LoginPage {
+    constructor(page) {
+        this.page = page;
+
+    }
+    async goto() {
+        await this.page.goto("https://stage-unientwww.euwest01.umbraco.io/");
+        await this.page.waitForLoadState('networkidle');
+
+    }
+
+    async login(username, password) {
+
+        //Enter Username Field
+        const usernameField = await this.page.getByLabel('User name');
+        await usernameField.fill(username);
+
+        //Enter Password field
+        const passwordField = await this.page.getByLabel('Password');
+        await passwordField.fill(password);
+
+
+        //Click Login button
+        const loginButton = await this.page.getByRole('button');
+        await loginButton.click();
+
+
+        //Click Accept All
+        const acceptAll = await this.page.locator("button[id='cookie-accept']");
+        await acceptAll.click();
+
+
+        //Automatically close the page
+        // await this.page.close();
+
+
+    }
+
+}
